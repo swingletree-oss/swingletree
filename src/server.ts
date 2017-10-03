c79c0ab17e4fee4341af0993a52253699053f21b
error418 <error418@users.noreply.github.com>
2017-10-03 21:00:31 +0200

    add docs

--- src/server.ts
+++ src/server.ts
@@ -8,19 +8,24 @@ import * as path from "path";
 
 const app = express();
 
+// express configuration
 app.set("port", process.env.PORT || 3000);
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// global event bus
 const eventBus = new EventEmitter();
 
+// application webhooks with plumbing
 const githubWebhook = new GitHubWebhook(eventBus);
 const sonarWebhook = new SonarWebhook(eventBus);
 
+// bind webhooks to paths
 app.use("/webhook/github/", githubWebhook.webhook);
 app.use("/webhook/sonar/", sonarWebhook.webhook);
 
+// kickstart everything
 app.listen(app.get("port"), () => {
   console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
   console.log("  Press CTRL-C to stop\n");
