fbf6302a44af1e11d56ae6f792a821b43b291da9
error418 <error418@users.noreply.github.com>
2018-04-21 01:50:35 +0200

    github app integration

--- src/github/commit-status-sender.ts
+++ src/github/commit-status-sender.ts
@@ -1,30 +1,57 @@
 "use strict";
 
+import { LOGGER } from "../logger";
+
 import { AppEvent } from "../app-events";
 import { GitHubGhCommitStatus, GitHubGhCommitStatusContainer } from "./model/gh-commit-status";
+import { GithubTokenFactory } from "./token/github-tokens";
 import { EventEmitter } from "events";
 
-const unirest = require("unirest");
+const Octokit = require("@octokit/rest");
 
 /** Sends Commit Status Requests to GitHub
  */
 export class CommitStatusSender {
 	private eventEmitter: EventEmitter;
 	private apiEndpoint: string;
+	private ghClient: any;
+	private tokenFactory: GithubTokenFactory;
 
-	constructor(eventEmitter: EventEmitter, apiEndpoint: string) {
+	constructor(eventEmitter: EventEmitter, apiEndpoint: string, tokenFactory: GithubTokenFactory) {
 		this.eventEmitter = eventEmitter;
 		this.apiEndpoint = apiEndpoint;
 		this.eventEmitter.on(AppEvent.sendStatus, this.sendStatus);
+		this.ghClient = Octokit({
+			baseUrl: apiEndpoint
+		});
+
+		this.tokenFactory = tokenFactory;
 	}
 
 	private sendStatus = (status: GitHubGhCommitStatusContainer) => {
-		unirest.post(this.apiEndpoint + "/repos/" + status.repository + "/statuses/" + status.commitId)
-			.headers({ "Accept": "application/json", "Content-Type": "application/json" })
-			.send(status.payload)
-			.end(function(response: any) {
-				this.eventEmitter.emit(AppEvent.statusSent, status, this.apiEndpoint);
-			}
-		);
+
+		const coordinates = status.repository.split("/");
+
+		this.ghClient.authenticate({
+			type: "integration",
+			token: this.tokenFactory.createJWT()
+		});
+
+		this.ghClient.repos.createStatus({
+			owner: coordinates[0],
+			repo: coordinates[1],
+			sha: status.commitId,
+			state: status.payload.state,
+			target_url: status.payload.target_url,
+			description: status.payload.description,
+			context: "swingletree"
+		})
+		.then(() => {
+			this.eventEmitter.emit(AppEvent.statusSent, status, this.apiEndpoint);
+		})
+		.catch((error: any) => {
+			LOGGER.error("could not persist status for %s", status.repository);
+			LOGGER.error(error);
+		});
 	}
 }
