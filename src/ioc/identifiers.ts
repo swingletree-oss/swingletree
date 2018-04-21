ab28e10fe81e2561704f37a9fd432d0be6c60ef4
error418 <error418@users.noreply.github.com>
2018-04-21 19:31:25 +0200

    refactoring to ioc

--- src/ioc/identifiers.ts
+++ src/ioc/identifiers.ts
@@ -1,7 +1,11 @@
 const Identifiers = {
 	ConfigurationService: Symbol.for("ConfigurationService"),
 	CommitStatusService: Symbol.for("CommitStatusService"),
-	GithubClientService: Symbol.for("GithubClientService")
+	GithubClientService: Symbol.for("GithubClientService"),
+	EventBus: Symbol.for("EventBus"),
+	SwingletreeServer: Symbol.for("SwingletreeServer"),
+	SonarWebhook: Symbol.for("SonarWebhook"),
+	GithubWebhook: Symbol.for("GithubWebhook")
 };
 
 export default Identifiers;
---
+++ src/swingletree.ts
@@ -0,0 +1,57 @@
+import CommitStatusSender from "./github/commit-status-sender";
+import GithubWebhook from "./github/github-webhook";
+import SonarWebhook from "./sonar/sonar-webhook";
+import * as express from "express";
+import * as compression from "compression";
+import * as bodyParser from "body-parser";
+import * as path from "path";
+import { Response, Request } from "express";
+
+import { LOGGER } from "./logger";
+import ConfigurationService from "./configuration";
+import { injectable } from "inversify/dts/annotation/injectable";
+import { inject } from "inversify/dts/annotation/inject";
+import Identifiers from "./ioc/identifiers";
+import EventBus from "./event-bus";
+
+@injectable()
+class SwingletreeServer {
+	private configurationService: ConfigurationService;
+	private githubWebhook: GithubWebhook;
+	private sonarWebhook: SonarWebhook;
+
+	constructor(
+		@inject(Identifiers.GithubWebhook) githubWebhook: GithubWebhook,
+		@inject(Identifiers.SonarWebhook) sonarWebhook: SonarWebhook,
+		@inject(Identifiers.ConfigurationService) configurationService: ConfigurationService
+	) {
+		this.githubWebhook = githubWebhook;
+		this.sonarWebhook = sonarWebhook;
+		this.configurationService = configurationService;
+	}
+
+	public run(app: any) {
+		// express configuration
+		app.set("port", process.env.PORT || 3000);
+		app.use(compression());
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: true }));
+
+		// bind webhooks to paths
+		app.post("/webhook/github/", this.githubWebhook.webhook);
+		app.post("/webhook/sonar/", this.sonarWebhook.webhook);
+
+
+		// health endpoint
+		app.get("/health", (request: Request, response: Response) => {
+			response.sendStatus(200);
+		});
+
+		// kickstart everything
+		app.listen(app.get("port"), () => {
+			LOGGER.info("listening on http://localhost:%d in %s mode", app.get("port"), app.get("env"));
+		});
+	}
+}
+
+export default SwingletreeServer;
