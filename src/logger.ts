4229e82f43b576920f339ef6e92c3383f7e42e56
error418 <error418@users.noreply.github.com>
2018-04-22 13:41:27 +0200

    fix logging

--- src/logger.ts
+++ src/logger.ts
@@ -2,7 +2,7 @@ import * as winston from "winston";
 
 export const LOGGER: winston.LoggerInstance = new (winston.Logger)(
 	{
-		level: (process.env["dev"]) ? "info" : "off",
+		level: (process.env.NODE_ENV == "test") ? "off" : "info",
 		transports: [new (winston.transports.Console)()]
 	}
 );
