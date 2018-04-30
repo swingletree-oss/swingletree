2378b1dc6b2ecaf62903c97120e5a51a95b1538d
error418 <error418@users.noreply.github.com>
2018-04-30 21:20:34 +0200

    chore: entrypoint escaping

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,10 @@
-printf $GH_APP_PEM > ./gh-app.pem
+echo "$GH_APP_PEM" > ./gh-app.pem
 
 if [ -z ${CONFIG+x} ]; then
     echo "INFO: container swingletree configuration will be used"
 else
     echo "INFO: using swingletree configuration from env var"
-    echo $CONFIG > ./swingletree.conf.yaml
+    printf $CONFIG > ./swingletree.conf.yaml
 fi
 
 node main.js $@
