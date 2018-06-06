6ac32ea48c0233f203c3613671c676d6225a12fc
error418 <error418@users.noreply.github.com>
2018-06-06 21:37:07 +0200

    chore: switch to `basic-auth`

--- src/sonar/sonar-webhook.ts
+++ src/sonar/sonar-webhook.ts
@@ -9,9 +9,8 @@ import { injectable } from "inversify";
 import { inject } from "inversify";
 import EventBus from "../event-bus";
 import { ConfigurationService } from "../configuration";
-import * as basicAuth from "express-basic-auth";
+import * as BasicAuth from "basic-auth";
 import { LOGGER } from "../logger";
-
 
 /** Provides a Webhook for Sonar
  */
@@ -39,19 +38,26 @@ class SonarWebhook {
 		return true; // TODO: check for analyze marker property in properties section // get target branch from there?
 	}
 
+	private authenticationMiddleware(secret: string) {
+		return (req: Request, res: Response, next: NextFunction) => {
+			const auth = BasicAuth(req);
+			if (auth && secret === auth.pass) {
+				next();
+			} else {
+				res.sendStatus(401);
+			}
+		};
+	}
+
 	public getRoute(): Router {
 		const router = Router();
 		const secret = this.configurationService.get().sonar.secret;
 
 		if (secret && secret.trim().length > 0) {
-			router.use(basicAuth({
-					users: { "webhook": secret }
-				})
-			);
+			router.use(this.authenticationMiddleware(secret));
 		} else {
 			LOGGER.warn("SonarQube webhook is not protected. Consider setting a sonar secret in the Swingletree configuration.");
 		}
-
 		router.post("/", this.webhook);
 
 		return router;
