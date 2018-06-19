2a3f95fc8ba4215a1b0cb31f6b3aa47ecca5d1df
error418 <error418@users.noreply.github.com>
2018-06-19 12:03:47 +0200

    chore(logger): add timestamp to logs

--- src/logger.ts
+++ src/logger.ts
@@ -3,7 +3,10 @@ import * as winston from "winston";
 export const LOGGER: winston.LoggerInstance = new (winston.Logger)(
 	{
 		level: process.env.LOG_LEVEL || "info",
-		transports: [new (winston.transports.Console)()]
+		transports: [new (winston.transports.Console)({
+			timestamp: true,
+			colorize: true
+		})],
 	}
 );
 
