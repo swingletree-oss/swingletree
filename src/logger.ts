410a83ae7f64c51530bb15ba2a1d36d6a33987b5
error418 <error418@users.noreply.github.com>
2018-10-31 10:52:48 +0100

    feat: add logging options for logstash and json output

--- src/logger.ts
+++ src/logger.ts
@@ -1,13 +1,45 @@
 import * as winston from "winston";
+import { Format } from "logform";
 
-export const LOGGER: winston.LoggerInstance = new (winston.Logger)(
+export enum LOG_FORMAT {
+	JSON = "json",
+	LOGSTASH = "logstash"
+}
+
+let format = winston.format.combine(
+	winston.format.timestamp(),
+	winston.format.splat()
+);
+
+if (process.env["LOG_FORMAT"] == LOG_FORMAT.JSON) {
+	format = winston.format.combine(
+		format,
+		winston.format.json()
+	);
+} else if (process.env["LOG_FORMAT"] == LOG_FORMAT.LOGSTASH) {
+	format = winston.format.combine(
+		format,
+		winston.format.logstash()
+	);
+} else {
+	if (process.env["LOG_DISABLE_COLORS"] == "true") {
+		format = winston.format.combine(format, winston.format.uncolorize());
+	} else {
+		format = winston.format.combine(format, winston.format.colorize());
+	}
+
+	format = winston.format.combine(format, winston.format.simple(), winston.format.printf(info => {
+		return `${info.timestamp} ${info.level}: ${info.message}`;
+	}));
+}
+
+export const LOGGER: winston.Logger = winston.createLogger(
 	{
 		level: process.env.LOG_LEVEL || "info",
-		transports: [new (winston.transports.Console)({
-			timestamp: true,
-			colorize: process.env.LOG_DISABLE_COLORS != "true"
-		})],
+		format: format,
+		transports: [new (winston.transports.Console)()],
 	}
 );
+
 
 LOGGER.info("Logger log level set to %s", LOGGER.level.toUpperCase());
