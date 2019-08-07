25549180ae454010ef71b7e1d31ecfaebb5fa5fb
Michael Gerbig <error418@users.noreply.github.com>
2019-08-07 21:09:58 +0200

    tests(twistlock): add report tests

--- test/twistlock/template.spec.ts
+++ test/twistlock/template.spec.ts
@@ -4,8 +4,10 @@ import { suite, test, describe } from "mocha";
 import { expect, assert } from "chai";
 import * as chai from "chai";
 import * as sinon from "sinon";
+chai.should();
 chai.use(require("sinon-chai"));
 chai.use(require("chai-as-promised"));
+chai.use(require("chai-things"));
 
 import { TemplateEngine, Templates } from "../../src/core/template/template-engine";
 import { TwistlockModel } from "../../src/twistlock/model";
@@ -36,11 +38,57 @@ describe("Twistlock Template", () => {
 		});
 
 		it("should run ZAP template with test data", () => {
+			const findingReport = new TwistlockModel.util.FindingReport(
+				testData,
+				TwistlockModel.FindingSeverity.LOW,
+				0,
+				TwistlockModel.FindingSeverity.LOW
+			);
+
 			const templateContent = uut.template<TwistlockModel.Template>(Templates.TWISTLOCK_SCAN, {
-				report: testData
+				report: testData,
+				issues: findingReport
 			});
 
 			expect(templateContent).to.contain("CVE-2019-5827", "expected CVE is missing");
+		});
+
+		it("should place vulnerability exceptions into dedicated value containers", () => {
+			const exceptions = new Map<string, string>();
+			exceptions.set("CVE-2019-5827", "is not exploitable");
+
+			const findingReport = new TwistlockModel.util.FindingReport(
+				testData,
+				TwistlockModel.FindingSeverity.LOW,
+				0,
+				TwistlockModel.FindingSeverity.LOW,
+				exceptions
+			);
+
+			expect(findingReport.vulnerabilityIssues).to.not.contain.something.with.property("id", "CVE-2019-5827");
+			expect(findingReport.ignoredVulnerabilityIssues).to.contain.something.with.property("id", "CVE-2019-5827");
+		});
+
+		it("should show vulnerability whitelists in report template", () => {
+			const exceptions = new Map<string, string>();
+			exceptions.set("CVE-2019-5827", "is not exploitable");
+
+			const findingReport = new TwistlockModel.util.FindingReport(
+				testData,
+				TwistlockModel.FindingSeverity.LOW,
+				0,
+				TwistlockModel.FindingSeverity.LOW,
+				exceptions
+			);
+
+			const templateContent = uut.template<TwistlockModel.Template>(Templates.TWISTLOCK_SCAN, {
+				report: testData,
+				issues: findingReport
+			});
+
+			expect(templateContent).to.contain("CVE-2019-5827");
+			expect(templateContent).to.contain("is not exploitable");
+			expect(templateContent).to.contain("Ignored Vulnerabilities");
 		});
 
 	});
