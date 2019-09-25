cbeda4048d3c17f640addf0946fef3c537f74dbd
error418 <error418@users.noreply.github.com>
2019-09-25 18:55:18 +0200

    fix(ui): improve metadata rendering

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -34,5 +34,27 @@ describe("ConfigurationService", () => {
 			expect(result["a.d.array.1"]).to.be.equal(2);
 			expect(result["a.d.array.2"]).to.be.equal(3);
 		});
+
+		it("should flatten objects and concat arrays", () => {
+			const testObj = {
+				a: {
+					b: "c",
+					d: {
+						e: "f",
+						g: "h",
+						array: [ 1, 2, 3 ]
+					}
+				}
+			};
+
+			const result = SwingletreeUtil.flattenObject(testObj, true);
+
+			console.log(result);
+
+			expect(result["a.d.e"]).to.be.equal("f");
+			expect(result["a.d.g"]).to.be.equal("h");
+			expect(result["a.b"]).to.be.equal("c");
+			expect(result["a.d.array"]).to.be.lengthOf(3);
+		});
 	});
 });
