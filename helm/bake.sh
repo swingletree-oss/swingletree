e4ba5fe0d9cae1e7f2e600a1b3d3fe8e7bcf7d2d
error418 <error418@users.noreply.github.com>
2019-11-16 17:13:30 +0100

    chore: add utility script for helm bake

---
+++ helm/bake.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+TEMP=`getopt -o vdm: --long gh-appid:,gh-keyfile: -- "$@"`
+
+if [ $? != 0 ] ; then echo "missing arguments. terminating..." >&2 ; exit 1 ; fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+GITHUB_KEYFILE=
+GITHUB_APPID=
+
+TARGET=swingletree-bake.yml
+
+while true; do
+  case "$1" in
+    --gh-keyfile ) GITHUB_KEYFILE="$2"; shift 2 ;;
+    --gh-appid ) GITHUB_APPID="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [ -z $GITHUB_APPID ]; then
+  echo " ! option --gh-appid is missing"
+  exit 1
+fi
+
+if [ -z $GITHUB_KEYFILE ]; then
+  echo " ! option --gh-keyfile is missing"
+  exit 1
+fi
+
+if [ -e $GITHUB_KEYFILE ]; then
+  echo " > baking your manifest into $TARGET"
+  helm template swingletree \
+    --set swingletree.scotty.github.app.id=$GITHUB_APPID \
+    --set-file github_app_key=$GITHUB_KEYFILE \
+    > $TARGET
+else
+  echo " ! $GITHUB_KEYFILE does not exist"
+  exit 1
+fi
