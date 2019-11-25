14c792a92850560c842664014042220587621820
error418 <error418@users.noreply.github.com>
2019-11-25 19:47:51 +0100

    chore(util): improve test data sender script

--- util/send-testdata.sh
+++ util/send-testdata.sh
@@ -4,23 +4,42 @@ BASEDIR=$(dirname "$0")
 
 BUILD_UUID=$(pwgen 20 1)
 
-NEBULA_TESTDATA=$BASEDIR/../../plugin-nebula/test/mock/nebula/call.json
+TEMP=`getopt -o o:,r:,s:,b: --long owner:,repo:,sha:,branch: -- "$@"`
+
+NEBULA_TESTDATA=$BASEDIR/../../plugin-nebula/test/mock/nebula/build-call.json
 TWISTLOCK_TESTDATA_ALL=$BASEDIR/../../plugin-twistlock/test/mock/twistlock-report-all.json
 TWISTLOCK_TESTDATA_CLEAN=$BASEDIR/../../plugin-twistlock/test/mock/twistlock-report-clean.json
 ZAP_TESTDATA=$BASEDIR/../../plugin-zap/test/mock/zap-report.json
 
-QUERY_PARAMS="?org=$1&repo=$2&sha=$3&branch=$3&uid=$BUILD_UUID"
+OWNER=
+REPO=
+SHA=
+BRANCH=
 
-#echo "send nebula"
-#curl -H "Content-Type: application/json" -X POST -d "@$NEBULA_TESTDATA" http://localhost:3001/report/nebula$QUERY_PARAMS
-#echo
+while true; do
+  case "$1" in
+    -o | --owner ) OWNER="$2"; shift 2 ;;
+    -r | --repo ) REPO="$2"; shift 2 ;;
+    -s | --sha ) SHA="$2"; shift 2 ;;
+    -b | --branch ) BRANCH="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+
+QUERY_PARAMS="?org=$OWNER&repo=$REPO&sha=$SHA&branch=$BRANCH&uid=$BUILD_UUID"
+
+echo "send nebula"
+curl -H "Content-Type: application/json" -X POST -d "@$NEBULA_TESTDATA" http://$1/report/nebula$QUERY_PARAMS
+echo
 
 echo "send twistlock"
-curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_ALL" http://localhost:3001/report/twistlock$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_ALL" http://$1/report/twistlock$QUERY_PARAMS
 echo
-curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_CLEAN" http://localhost:3001/report/twistlock$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_CLEAN" http://$1/report/twistlock$QUERY_PARAMS
 echo
 
 echo "send zap"
-curl -H "Content-Type: application/json" -X POST -d "@$ZAP_TESTDATA" http://localhost:3001/report/zap$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$ZAP_TESTDATA" http://$1/report/zap$QUERY_PARAMS
 echo
