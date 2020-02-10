93f2ce92da1378d1e83646cab22ce4b372cb5982
error418 <error418@users.noreply.github.com>
2020-02-10 11:39:27 +0100

    chore(util): improve test data send script

--- util/send-testdata.sh
+++ util/send-testdata.sh
@@ -4,12 +4,13 @@ BASEDIR=$(dirname "$0")
 
 BUILD_UUID=$(pwgen 20 1)
 
-TEMP=`getopt -o o:,r:,s:,b: --long owner:,repo:,sha:,branch: -- "$@"`
+TEMP=`getopt -o o:,r:,s:,u:,b: --long owner:,repo:,sha:,branch: -- "$@"`
 
 NEBULA_TESTDATA=$BASEDIR/../../plugin-nebula/test/mock/nebula/build-call.json
 TWISTLOCK_TESTDATA_ALL=$BASEDIR/../../plugin-twistlock/test/mock/twistlock-report-all.json
 TWISTLOCK_TESTDATA_CLEAN=$BASEDIR/../../plugin-twistlock/test/mock/twistlock-report-clean.json
 ZAP_TESTDATA=$BASEDIR/../../plugin-zap/test/mock/zap-report.json
+TESTNG_TESTDATA=$BASEDIR/../../plugin-testng/test/mock/testng-report.xml
 
 OWNER=
 REPO=
@@ -22,6 +23,7 @@ while true; do
     -r | --repo ) REPO="$2"; shift 2 ;;
     -s | --sha ) SHA="$2"; shift 2 ;;
     -b | --branch ) BRANCH="$2"; shift 2 ;;
+    -u ) URL="$2"; shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -31,15 +33,19 @@ done
 QUERY_PARAMS="?org=$OWNER&repo=$REPO&sha=$SHA&branch=$BRANCH&uid=$BUILD_UUID"
 
 echo "send nebula"
-curl -H "Content-Type: application/json" -X POST -d "@$NEBULA_TESTDATA" http://$1/report/nebula$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$NEBULA_TESTDATA" http://$URL/report/nebula$QUERY_PARAMS
 echo
 
 echo "send twistlock"
-curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_ALL" http://$1/report/twistlock$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_ALL" http://$URL/report/twistlock$QUERY_PARAMS
 echo
-curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_CLEAN" http://$1/report/twistlock$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$TWISTLOCK_TESTDATA_CLEAN" http://$URL/report/twistlock$QUERY_PARAMS
 echo
 
 echo "send zap"
-curl -H "Content-Type: application/json" -X POST -d "@$ZAP_TESTDATA" http://$1/report/zap$QUERY_PARAMS
+curl -H "Content-Type: application/json" -X POST -d "@$ZAP_TESTDATA" http://$URL/report/zap$QUERY_PARAMS
+echo
+
+echo "send testng"
+curl -H "Content-Type: application/xml" -X POST -d "@$TESTNG_TESTDATA" http://$URL/report/testng$QUERY_PARAMS
 echo
