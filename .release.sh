b471ed1267cc8725492381ca53344b92667de307
error418 <error418@users.noreply.github.com>
2020-03-17 18:32:06 +0100

    chore(helm): set chart version to release tag version

--- .release.sh
+++ .release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
 
-helm package ${BASEDIR}/helm/swingletree --app-version $1 --dependency-update -d ${BASEDIR}/helm/dist
+helm package ${BASEDIR}/helm/swingletree --app-version $1 --version $1 --dependency-update -d ${BASEDIR}/helm/dist
