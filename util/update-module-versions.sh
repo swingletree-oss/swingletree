f512d3f5cd41d0f8e0afffe53eccdec50aca5b31
error418 <error418@users.noreply.github.com>
2020-03-27 13:41:05 +0100

    chore(util): determine semrel version for component upgrades

--- util/update-module-versions.sh
+++ util/update-module-versions.sh
@@ -7,7 +7,7 @@ BASEDIR=$(dirname "$0")
 VALUES_YAML=$BASEDIR/../helm/swingletree/values.yaml
 
 function addCommit {
-  CMESSAGE="feat($1): update component from \`$2\` to \`$3\` [view changes](https://github.com/swingletree-oss/$1/compare/v$2...v$3)"
+  CMESSAGE="$SCOPE($1): update component from \`$2\` to \`$3\` [view changes](https://github.com/swingletree-oss/$1/compare/v$2...v$3)"
   echo " > adding commit:   $CMESSAGE"
 
   git add $VALUES_YAML
@@ -37,13 +37,43 @@ COMPONENTS=(
   'plugin-testng'
 )
 
+VER_REGEX='([0-9]+)\.([0-9]+)\.([0-9]+)'
+
 for repo in "${COMPONENTS[@]}"; do
   CURRENT=$( currentRelease $repo )
   LATEST=$( latestRelease $repo )
   if [ "$CURRENT" == "$LATEST" ]; then
     printf " OK       %-20s %-10s    %-10s is already on latest version\n" $repo $CURRENT
   else
-    printf " UPGRADE  %-20s %-10s -> %-10s needs upgrade\n" $repo $CURRENT $LATEST
+
+    if [[ $CURRENT =~ $VER_REGEX ]]; then
+      CURRENT_MAJOR=${BASH_REMATCH[1]}
+      CURRENT_MINOR=${BASH_REMATCH[2]}
+      CURRENT_BUGFIX=${BASH_REMATCH[3]}
+    fi
+
+    declare -a GRP_LATEST
+    if [[ $LATEST =~ $VER_REGEX ]]; then
+      LATEST_MAJOR=${BASH_REMATCH[1]}
+      LATEST_MINOR=${BASH_REMATCH[2]}
+      LATEST_BUGFIX=${BASH_REMATCH[3]}
+    fi
+
+    SCOPE=
+    if [ "${CURRENT_BUGFIX}" != "${LATEST_BUGFIX}" ]; then
+      SCOPE="fix"
+    fi
+
+    if [ "${CURRENT_MINOR}" != "${LATEST_MINOR}" ]; then
+      SCOPE="feat"
+    fi
+
+    if [ "${CURRENT_MAJOR}" != "${LATEST_MAJOR}" ]; then
+      SCOPE="feat"
+    fi
+
+    printf " UPGRADE  %-20s %-10s -> %-10s needs upgrade ($SCOPE)\n" $repo $CURRENT $LATEST
+
     upgrade $repo $LATEST
     addCommit $repo $CURRENT $LATEST
   fi
