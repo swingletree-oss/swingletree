4f37ab63e879e81050b6c1f0f3663767e904e810
error418 <error418@users.noreply.github.com>
2020-07-17 15:09:31 +0200

    chore: update package updater

--- util/update-packages.sh
+++ util/update-packages.sh
@@ -3,19 +3,24 @@
 BASEDIR=$(dirname "$0")
 
 COMPONENTS=(
-  gate
   deck
-  scotty
+  gate
+  harness
+  plugin-junit
   plugin-nebula
   plugin-sonarqube
+  plugin-testng
   plugin-twistlock
   plugin-zap
+  scotty
+  scotty-client
 )
 
 for repo in "${COMPONENTS[@]}"; do
   pushd $BASEDIR/../../$repo
   npm update --save
   npm update --save-dev
+  npm audit --fix
   npm test
   popd
 done
